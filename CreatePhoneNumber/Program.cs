4b35dfce4876b6090887af1b322f5aa3b86050dd
gpietrzak-pl <kontakt@gpietrzak.pl>
2021-09-01 18:58:16 +0200

    Add project files.

---
+++ CreatePhoneNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CreatePhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    /// <summary>
+    /// https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/csharp
+    /// </summary>
+    public class Kata
+    {
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            string str = ($"({numbers[0]}{numbers[1]}{numbers[2]}) {numbers[3]}{numbers[4]}{numbers[5]}-{numbers[6]}{numbers[7]}{numbers[8]}{numbers[9]}");
+            return str;
+        }
+    }
+}
