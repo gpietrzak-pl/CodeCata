c74db979a0dd4b15a1da0ff7c489e6b845e51c95
gpietrzak-pl <kontakt@gpietrzak.pl>
2021-09-07 18:52:00 +0200

    Moving Zeros To The End added

---
+++ Moving Zeros To The End/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Moving_Zeros_To_The_End
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/csharp
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int[] MoveZeroes(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] == 0)
+                    {
+                        int tmp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = tmp;
+                    }
+                }
+
+            }
+
+            return arr;
+        }
+    }
+}
