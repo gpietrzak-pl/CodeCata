8960b20af2c83de9b38d91f620a5a2010eefd8b7
gpietrzak-pl <kontakt@gpietrzak.pl>
2021-09-07 18:35:21 +0200

    Friend or Foe added

---
+++ Array.diff/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Array.diff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    /// <summary>
+    /// https://www.codewars.com/kata/523f5d21c841566fde000009/train/csharp
+    /// </summary>
+    public class Kata
+    {
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+           foreach(int item in b)
+            {
+                foreach(int item2 in a)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ Friend or Foe/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Friend_or_Foe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// https://www.codewars.com/kata/55b42574ff091733d900002f/train/csharp
+        /// </summary>
+        public static class Kata
+        {
+            public static IEnumerable<string> FriendOrFoe(string[] names)
+            {
+                var output = new List<string>();
+
+                foreach (var item in names)
+                {
+                    if (item.Length == 4)
+                        output.Add(item);
+                }
+
+                return output;
+            }
+        }
+    }
+}
