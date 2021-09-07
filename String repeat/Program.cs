e9a257dd9d4920408e38e4d245e49a177c9d426a
gpietrzak-pl <kontakt@gpietrzak.pl>
2021-09-07 18:44:29 +0200

    String repeat added

---
+++ String repeat/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace String_repeat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static class Repeat
+        {
+            public static string RepeatStr(int n, string s)
+            {
+                var output = "";
+                for (int i = 0; i < n; i++)
+                {
+                    //String.Empty
+                    output = output + s;
+                }
+                return output;
+            }
+        }
+    }
+}
